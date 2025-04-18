bac4d7fc5b8ebf48ad1a0c7ec4e4ef6ae4d5f45b
Layamed-Consulting <mehdi.benjebara@outlook.com>
2025-04-18 11:53:09 +0100

    layamedcommit

---
+++ layamed_consulting_module/__manifest__.py
@@ -0,0 +1,45 @@
+{
+    'name': "LAYAMED Consulting",
+    'description': "Layamed consulting",
+    'summary': "",
+    'author': 'ST',
+    'category': 'base',
+    'version': '1.0',
+    'description': """
+        This module introduces custom features for the Chic Corner system, including new models, views, and logic to improve functionality.
+    """,
+    'author': 'LAYAMEDCONSULTING',
+    'website': 'http://www.layamedconsulting.com',
+    'category': '',
+    'depends': ['base', 'sale','website' ],
+    'data': [
+        'report/devis.xml',
+        'report/BC.xml',
+        'views/invoice.xml',
+        'views/hr_payslip_view.xml',
+        'report/hr_payslip_report.xml',
+        'views/website_homepage_inherit.xml',
+    ],
+    'assets': {
+        'web.assets_frontend': [
+            'layamed_custom_module/static/src/js/references_slider.js',
+            'layamed_custom_module/static/src/css/reference_website.css',
+        ],
+    },
+    #'assets': {
+        #'web.report_assets_common': [
+            #'chic_corner_module/static/src/css/bon_sortie_css.css',
+            #'chic_corner_module/static/src/css/style.css',
+        #],
+        #'web.assets_backend': [
+         #  'chic_corner_module/static/src/models/barcode_picking.js',
+           #'chic_corner_module/static/src/js/prnt_direct.js',
+        #],
+        #'point_of_sale._assets_pos': [
+         #  'chic_corner_module/static/src/app/**/*',
+        #],
+    #},
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ layamed_consulting_module/models/hr_payslip_inherit.py
@@ -0,0 +1,25 @@
+from odoo import models, fields, api
+
+class HrPayslip(models.Model):
+    _inherit = "hr.payslip"
+
+    def action_custom_payslip(self):
+        """Custom logic for generating the custom payslip"""
+        return self.env.ref('layamed_custom_module.report_custom_payslip').report_action(self)
+
+
+class HrPayslipLine(models.Model):
+    _inherit = 'hr.payslip.line'
+
+    parameter_value = fields.Text(string="Taux", compute="_compute_parameter_value", store=True)
+
+    @api.depends('slip_id.line_ids.total', 'category_id')
+    def _compute_parameter_value(self):
+        for line in self:
+            if line.category_id.code == "UNTAXABLE_DED":  # Check if it's a deduction
+                basic_salary = next(
+                    (l.total for l in line.slip_id.line_ids if l.category_id.code == "BASIC"), 0.0
+                )
+                line.parameter_value = round((line.total / basic_salary) * 100, 2) if basic_salary else False
+            else:
+                line.parameter_value = False
